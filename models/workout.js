1766055ea32fda9e0650d3f000ce1a2852bc7879
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-03-31 00:22:07 -0400

    finished workout model

--- models/workout.js
+++ models/workout.js
@@ -2,5 +2,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [{
+        type: {
+            type: String
+        },
+        exerciseType: {
+            type: String
+        },
+        name: {
+            type: String
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        }
+    }]
 });
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
