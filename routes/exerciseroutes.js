cf603941898e20229001ee8e1be0c08a255d8de3
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-04-02 16:58:40 -0400

    undid all changes

--- routes/exerciseroutes.js
+++ routes/exerciseroutes.js
@@ -3,13 +3,9 @@ const db = require("../models");
 const Workout = db.Workout
 
 module.exports = function (app) {
-    app.get('/api/test', (req, res) => {
-        res.send('<h1>You have hit the server</h1>')
-    })
 
     app.get("/api/workouts", function (req, res) {
         Workout.find({}).then(data => {
-
                 res.json(data)
             })
             .catch(err => {
@@ -18,52 +14,31 @@ module.exports = function (app) {
     })
     app.put("/api/workouts/:id", function (req, res) {
         console.log(" PARAMS ID", req.params.id)
-        /*  Workout.findOne({
-             _id: req.params.id
-         }, function (err, workout) {
-             console.log("Workout object -->", workout)
-             workout.exercises.push(req.body)
-
-             Workout.updateOne({
-                 _id: req.params.id
-             }, workout, function (err, data) {
-                 if (err) throw err
-                 res.json(data)
-             })
-         }) */
-
-        Workout.updateOne({
+        Workout.findOne({
             _id: req.params.id
-        }, workout, (err, data) => {
-            console.log('UPDATE response => ', data)
-            if (err) {
-                console.log(err)
-            }
+        }, function (err, workout) {
+            console.log("Workout object -->", workout)
+            workout.exercises.push(req.body)
 
-            res.json(data)
+            Workout.updateOne({
+                _id: req.params.id
+            }, workout, function (err, data) {
+                if (err) throw err
+                res.json(data)
+            })
         })
-
-
     });
 
-    app.post("/api/workouts", async (req, res) => {
+    app.post("/api/workouts", function (req, res) {
         console.log("From Workouts -->", req.body)
 
-        const newWorkout = await new Workout(req.body)
+        const newWorkout = new Workout(req.body)
         newWorkout.save(function (err) {
             if (err) {
                 console.log(err)
             }
             res.json(newWorkout)
         })
-
-
-        /*    Workout.create(req.body, function (err, data) {
-               if (err) return console.log(err);
-               // saved!
-               res.json(data)
-             }); */
-
     });
 
     app.get("/api/workouts/range", function (req, res) {
